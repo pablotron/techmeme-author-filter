1b798aa2af9f5034c276444fac05b141bfac6223
pabs <pabs@pablotron.org>
2009-02-21 04:22:35 -0500

    add all of seans changes (edit button line, handle blanks, etc)

--- techmeme-author-filter.user.js
+++ techmeme-author-filter.user.js
@@ -6,95 +6,138 @@
 // ==/UserScript==
 
 (function () {
-  var key = 'author_filters', sum = 0, get, find, add_btn,
+  var key = 'author_filters', sum = 0, show_btn,
       F = Array.prototype.filter, classes = ['item', 'heditem'];
 
   /*
    * get - get filter words as string.
    */
-  get = function() { 
-    return GM_getValue(key) || ''; 
+  var get = function() { 
+    return (GM_getValue(key) || '').replace(/^\s*|\s*$/g, ''); 
   };
 
   /*
    * find - find matching article elements and pass to callback.
    */
-  find = (function() { 
-    // convert filter words into regular expressions (cached)
-    var res = get().split(/\s+/).map(function(str) {
+  var find = function(fn) {
+    // convert filter words into regular expressions
+    var res = get().split(/\s+/).filter(function(str) {
+      return str && str.match(/\S/);
+    }).map(function(str) {
       return new RegExp(str, 'i');
     });
-    
-    return function(fn) {
-      // find matching cite elements
-      F.call(document.getElementsByTagName('cite'), function(el) {
-        var str = el.innerHTML || '';
+  
+    // find matching cite elements
+    F.call(document.getElementsByTagName('cite'), function(el) {
+      var str = el.innerHTML || '';
 
-        return res.length && res.some(function(re) { return str.match(re); });
-      }).forEach(function(el) {
-        // walk up to containing div.item element and pass it to callback
-        while (el && (el = el.parentNode)) {
-          if (el.tagName == 'DIV' && classes.indexOf(el.className) != -1) {
-            fn(el);
-            el = null;
-          }
+      return res.length && res.some(function(re) { return str.match(re); });
+    }).forEach(function(el) {
+      // walk up to containing div.item element and pass it to callback
+      while (el && (el = el.parentNode)) {
+        if (el.tagName == 'DIV' && classes.indexOf(el.className) != -1) {
+          fn(el);
+          el = null;
         }
-      });
-    };
-  })();
-
-  /*
-   * add_btn - add "Show Hidden" button to top-right corner 
-   */
-  add_btn = function() {
-    var bd = 'Show Hidden (' + sum + ')',
-        el = document.createElement('button');
-
-    // move button to top-right corner
-    el.innerHTML = bd;
-    el.style.zIndex = 9999;
-    el.style.position = 'absolute';
-    el.style.top = '5px';
-    el.style.right = '5px';
-
-    // add click handler
-    el.addEventListener('click', function(ev) {
-      // show hidden articles
-      find(function(el) { 
-        el.style.display = 'block'; 
-      });
-
-      // hide button
-      el.style.display = 'none';
-
-      // stop event
-      return false;
-    }, false);
-
-    // add show button
-    document.body.appendChild(el);
+      }
+    });
   };
 
-  // add configuration menu
-  GM_registerMenuCommand('Edit Filters...', function() {
-    var msg = 'Edit Filters (list of space-separated words):',
-        val = get();
+  /*
+   * show - show all matching articles
+   */
+  var show = function() {
+    // show hidden articles
+    find(function(el) { 
+      el.style.display = 'block'; 
+    });
 
-    // prompt for and save filters
-    if (val = prompt(msg, val))
-      GM_setValue(key, val);
-  });
+    // reset sum
+    sum = 0;
 
-  // add window.load handler
-  window.addEventListener('load', function() {
+    // disable show btn
+    show_btn.style.display = 'none';
+    show_btn.innerHTML = 'Show All (0)';
+  };
+
+  /*
+   * hide - hide all matching articles 
+   */
+  var hide = function() {
     // hide matching items
     find(function(el) {
       el.style.display = 'none';
       sum++;
     });
 
-    // add show button if we hid anything
-    if (sum > 0)
-      add_btn();
+    // enable show button if we hid anything
+    if (sum > 0) {
+      show_btn.style.display = 'inline';
+      show_btn.innerHTML = 'Show All (' + sum + ')';
+    }
+  };
+
+  var edit = function() {
+    var msg = 'Edit Filters (list of space-separated words):',
+        val = get();
+
+    // prompt for and save filters
+    if (val = prompt(msg, val)) {
+      // show everything that was hidden
+      show();
+
+      // apply new value to config
+      GM_setValue(key, val);
+
+      // reset sum and hide all matching items
+      hide();
+    }
+  };
+
+  /*
+   * init - add config buttons to top-right corner 
+   */
+  var init = function() {
+    var btn, div = document.createElement('div');
+
+    // move div to top-right corner
+    div = document.createElement('div');
+    div.style.zIndex = 9999;
+    div.style.position = 'absolute';
+    div.style.top = '5px';
+    div.style.right = '8px';
+
+    // create show button
+    btn = show_btn = document.createElement('button');
+    btn.style.display = 'none';
+    btn.innerHTML = 'Show All (0)';
+    div.appendChild(btn);
+
+    // add show btn handler
+    btn.addEventListener('click', function(ev) {
+      // show hidden articles  and stop event
+      show();
+      return false;
+    }, false);
+
+    // create edit button
+    btn = document.createElement('button');
+    btn.innerHTML = 'Edit Filters';
+    btn.style.marginLeft = '5px';
+    btn.addEventListener('click', edit, false);
+    div.appendChild(btn);
+
+    // add buttons to screen
+    document.body.appendChild(div);
+  };
+
+  // init buttons, add configuration menu item, and add window load handler
+  GM_registerMenuCommand('Edit Filters...', edit);
+  window.addEventListener('load', function() {
+    // add config buttons
+    init();
+
+    // hide matching articles
+    hide();
   }, false);
 })();
